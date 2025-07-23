2db2b955fa1f89679f90093f785539bcca61cd7b
Lucas Leung <kayauleung2024@gmail.com>
2025-07-23 17:54:18 +0800

    test 2

---
+++ Script13.js
@@ -0,0 +1,40 @@
+const TextInput = document.querySelector('.TextInput')
+const Lists = document.querySelector('.Lists')
+const Button = document.querySelector('.Button')
+
+function NewTasks() {
+if(TextInput.value===''){
+    return
+}
+const Tasks = document.createElement('li')
+Tasks.innerHTML = ` <input type = 'checkbox' class='Checkbox'>
+<lable>${TextInput.value}</lable>
+<button class = 'Trash'>X</button>
+`
+const Trash = Tasks.querySelector('.Trash')
+const Checkbox = Tasks.querySelector('.Checkbox')
+Trash.addEventListener('click', function(){
+Tasks.remove()
+})
+Checkbox.addEventListener('change', function(){
+if(Checkbox.checked){
+    Tasks.style.textDecoration = 'line-through'
+    Tasks.style.color = '#999'
+    Lists.append(Tasks)
+}else{
+    Tasks.style.textDecoration = 'none'
+    Tasks.style.color = ''
+    Lists.prepend(Tasks)
+}
+})
+Lists.append(Tasks)
+TextInput.value = ''
+} 
+
+Button.addEventListener('click', NewTasks)
+
+TextInput.addEventListener('keyup', function(e)  {
+    if (e.key === 'Enter' ){
+        NewTasks()
+    }
+})
