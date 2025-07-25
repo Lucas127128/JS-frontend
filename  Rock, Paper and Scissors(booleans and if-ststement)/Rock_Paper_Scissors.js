80ead3b5c67f72082153725409ccb32f2bba64b5
Lucas Leung <kayauleung2024@gmail.com>
2025-07-25 19:48:16 +0800

    Let the user choose and decide who win or lose.

---  Rock, Paper and Scissors(booleans and if-ststement)/Rock_Paper_Scissors.js
+++  Rock, Paper and Scissors(booleans and if-ststement)/Rock_Paper_Scissors.js
@@ -1,6 +1,41 @@
 const Rock = document.querySelector('.Rock')
 const Paper = document.querySelector('.Paper')
 const Scissors = document.querySelector('.Scissors')
-const RockPaperScissors = [Rock, Paper, Scissors];
-const randomIndex = Math.floor(Math.random * myArray.length);
-const ComputerChoice = RockPaperScissors[randomIndex]
+const RockPaperScissors = ['Rock', 'Paper', 'Scissors'];
+const randomIndex = Math.floor(Math.random() * RockPaperScissors.length);
+let ComputerChoice = RockPaperScissors[randomIndex]
+let UserChoice = ''
+let Result = ''
+function DecideWhoWin(){
+    if(ComputerChoice===UserChoice){
+        Result = "Tie!"
+    }else if(ComputerChoice==='Rock' && UserChoice==='Scissors'){
+        Result = 'You lose!'
+    }else if(ComputerChoice==='Rock' && UserChoice==='Paper'){
+        Result = 'You win!'
+    }else if(ComputerChoice==='Scissors' && UserChoice==='Paper'){
+        Result = 'You lose!'
+    }else if(ComputerChoice==='Scissors' && UserChoice==='Rock'){
+        Result = 'You win!'
+    }else if(ComputerChoice==='Paper' && UserChoice==='Rock'){
+        Result = 'You lose!'
+    }else if(ComputerChoice==='Paper' && UserChoice==='Scissors'){
+        Result = 'You win!'
+    }
+    window.alert(`${Result}
+The computer choice is ${ComputerChoice}.`)
+
+}
+Rock.addEventListener('click', function(){
+UserChoice = 'Rock'
+DecideWhoWin()
+})
+Paper.addEventListener('click', function(){
+UserChoice = 'Paper'
+DecideWhoWin()
+})
+Scissors.addEventListener('click', function(){
+UserChoice = 'Scissors'
+DecideWhoWin()
+})
+console.log(ComputerChoice)
