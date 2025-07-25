7d622dd59ef2f5f0c5c1d1a819b3605ce4b41592
Lucas Leung <kayauleung2024@gmail.com>
2025-07-25 19:18:24 +0800

    Let the computer choose randomly

---
+++  Rock, Paper and Scissors(booleans and if-ststement)/Rock_Paper_Scissors.js
@@ -0,0 +1,6 @@
+const Rock = document.querySelector('.Rock')
+const Paper = document.querySelector('.Paper')
+const Scissors = document.querySelector('.Scissors')
+const RockPaperScissors = [Rock, Paper, Scissors];
+const randomIndex = Math.floor(Math.random * myArray.length);
+const ComputerChoice = RockPaperScissors[randomIndex]
