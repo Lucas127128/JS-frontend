ba432642f4b1ee0e52d5140497f5d33315c5f3e6
Lucas Leung <kayauleung2024@gmail.com>
2025-07-30 12:48:10 +0800

    creat score feature and local storage function for coin flip game

--- coin flip game(local storage)/coin_flip_game.js
+++ coin flip game(local storage)/coin_flip_game.js
@@ -3,15 +3,34 @@ const Result = randomNumber < 0.5 ? 'Heads' : 'Tails';
 const Tails = document.querySelector('.Tails')
 const Heads = document.querySelector('.Heads')
 let Guess = '';
+localStorage.getItem('Score')
+let Score = JSON.parse(
+localStorage.getItem('Score')
+)
+if (Score === null){
+        let Score = {
+        Wins: 0,
+        Losses: 0
+    }
+}
+function DecideWhoWin(){
+    console.log(Guess === Result ? 'You win!' : 'You lose!');
+    console.log(Guess)
+    console.log(Result)
+    if (Result === Guess){
+        Score.Wins += 1
+    }else {
+        Score.Losses += 1
+    }
+    localStorage.setItem('Score', JSON.stringify(Score))
+    //console.log(localStorage.getItem('Score'))
+    console.log(Score)
+}
 Tails.addEventListener('click', function(){
 Guess = 'Tails'
-console.log(Guess === Result ? 'You win!' : 'You lose!');
-console.log(Guess)
-console.log(Result)
+DecideWhoWin()
 })
 Heads.addEventListener('click', function(){
 Guess = 'Heads'
-console.log(Guess === Result ? 'You win!' : 'You lose!');
-console.log(Guess)
-console.log(Result)
+DecideWhoWin()
 })
