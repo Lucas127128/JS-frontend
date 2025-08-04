6fda961664c47e89931740fcc4d975f0244c0220
Lucas Leung <kayauleung2024@gmail.com>
2025-08-05 00:18:56 +0800

    Display the To do items and dates to the To do list website (array)

--- To-do-list-web-application/To-do-list-web-applicatio(array)/To-do-list-web-application(array).js
+++ To-do-list-web-application/To-do-list-web-applicatio(array)/To-do-list-web-application(array).js
@@ -1,10 +1,34 @@
 const Button = document.querySelector(".Button")
 const DueDate =document.querySelector(".DueDate")
 const ToDoItem =document.querySelector(".TodoItem")
-
+let TodoItems =''
+let DueDates = ''
+const Tasks = document.createElement('li')
 let TodoItemAndDate = [
     //{TodoDate: "",ToDoItem: ""}
     ]
+
+function DisplayTodoItems() {
+    for(let i =0; i<TodoItemAndDate.length; i++) {
+         TodoItems = TodoItemAndDate[i].ToDoItem
+         DueDates = TodoItemAndDate[i].TodoDate
+    }
+     Tasks.innerHTML = `
+     <div class="todo-row">
+        <div>${ToDoItem}</div>
+        <div>${DueDates}</div>
+        <button class="DeleteTodoItems" onclick="
+        TodoItemAndDate.splice(i, 1);
+        DisplayTodoItems();
+        ">
+        Delete
+        </button>
+      </div>
+     `
+     
+
+}
+    
 Button.addEventListener('click' ,function(){
     if(ToDoItem.value===''){
         alert('Please type todo items.')
