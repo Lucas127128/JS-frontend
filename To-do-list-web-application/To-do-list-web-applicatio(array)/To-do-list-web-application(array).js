d043a3815e70e60cb8498ddce6f5c1ac19bdd401
Lucas Leung <kayauleung2024@gmail.com>
2025-08-05 01:40:29 +0800

    Display the To do items and dates to the To do list website (array)(finally work)

--- To-do-list-web-application/To-do-list-web-applicatio(array)/To-do-list-web-application(array).js
+++ To-do-list-web-application/To-do-list-web-applicatio(array)/To-do-list-web-application(array).js
@@ -1,43 +1,46 @@
 const Button = document.querySelector(".Button")
 const DueDate =document.querySelector(".DueDate")
 const ToDoItem =document.querySelector(".TodoItem")
+const DisplayTheTodoList = document.querySelector('.DisplayTheTodoList')
 let TodoItems =''
 let DueDates = ''
-const Tasks = document.createElement('li')
 let TodoItemAndDate = [
-    //{TodoDate: "",ToDoItem: ""}
-    ]
-
+    //{TodoDate: "",TheTodoItems: ""}
+]
 function DisplayTodoItems() {
+    DisplayTheTodoList.innerHTML = ''
     for(let i =0; i<TodoItemAndDate.length; i++) {
-         TodoItems = TodoItemAndDate[i].ToDoItem
+         TodoItems = TodoItemAndDate[i].TheTodoItems
          DueDates = TodoItemAndDate[i].TodoDate
+         DisplayTheTodoList.innerHTML += `
+        <div>
+            <div>${TodoItems}</div>
+            <div>${DueDates}</div>
+            <button class="DeleteTodoItems" onclick = "
+            DeleteTodoItems(${i})
+            ">
+            Delete
+            </button>
+        </div>
+        `
     }
-     Tasks.innerHTML = `
-     <div class="todo-row">
-        <div>${ToDoItem}</div>
-        <div>${DueDates}</div>
-        <button class="DeleteTodoItems" onclick="
-        TodoItemAndDate.splice(i, 1);
-        DisplayTodoItems();
-        ">
-        Delete
-        </button>
-      </div>
-     `
-     
 
 }
-    
+function DeleteTodoItems(index){
+    TodoItemAndDate.splice(index, 1);
+    DisplayTodoItems();
+}   
 Button.addEventListener('click' ,function(){
     if(ToDoItem.value===''){
         alert('Please type todo items.')
         return;
     }
     TodoItemAndDate.push(
-        {TodoDate: DueDate.value ,ToDoItem: ToDoItem.value}
+        {TodoDate: DueDate.value ,TheTodoItems: ToDoItem.value}
     )
+    DisplayTodoItems()
     ToDoItem.value=''
+    DueDate.value = ''
     console.log(TodoItemAndDate)
 })
 console.log(TodoItemAndDate)
