460623368fe49132131b45081cc6af8d25c230e9
Lucas Leung <kayauleung2024@gmail.com>
2025-08-15 16:49:53 +0800

    Display added after add a product in Amazon project

--- Amazon_Project/Scripts/Amazon.js
+++ Amazon_Project/Scripts/Amazon.js
@@ -23,7 +23,7 @@ Products.forEach((products)=>{ProductsHTML += `
             </div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class = "ProductQuantitySelector">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -39,7 +39,7 @@ Products.forEach((products)=>{ProductsHTML += `
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart added-to-cart-${products.id}">
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
@@ -52,12 +52,18 @@ Products.forEach((products)=>{ProductsHTML += `
         </div>
     `
 })
+let ProductQuantity = 0
+let ProductId = ''
 const ProductsGrid = document.querySelector('.products-grid')
-ProductsGrid.innerHTML = ProductsHTML
+ProductsGrid.innerHTML = ProductsHTML 
 let AddToCartButton = document.querySelectorAll('.add-to-cart-button')
 AddToCartButton.forEach((Button) => {
     Button.addEventListener('click', ()=>{
-        let ProductId = Button.dataset.productId
+        ProductId = Button.dataset.productId
+        clearTimeout()
+        const ProductContainer = Button.closest('.product-container');
+        const QuantitySelector = ProductContainer.querySelector('.ProductQuantitySelector');
+        const QuantityToAdd = parseInt(QuantitySelector.value);
         let  MatchingItem 
         Cart.forEach((Item)=>{
             if(ProductId===Item.ProductId){
@@ -69,14 +75,20 @@ AddToCartButton.forEach((Button) => {
         }else{
             Cart.push({
             ProductId: ProductId,
-            Quantity: 1
+            Quantity: QuantityToAdd
         })
         }
         let CartQuantity=0;
         Cart.forEach((Item)=>{
             CartQuantity+=Item.Quantity
+            QuantitySelector.value=Item.Quantity
         })
         const cartQuantity = document.querySelector('.cart-quantity')
         cartQuantity.innerHTML = CartQuantity
+        const Added_To_Cart = document.querySelector(`.added-to-cart-${ProductId}`)
+        Added_To_Cart.classList.add("display-added-to-cart")
+        setTimeout(()=>{
+            Added_To_Cart.classList.remove("display-added-to-cart")
+        },1500)
     })
 })
