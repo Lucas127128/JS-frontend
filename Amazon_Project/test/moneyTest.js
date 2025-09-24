26d59a2476297b781c0664ad166b29fdd8ab329a
Lucas Leung <kayauleung2024@gmail.com>
2025-09-24 14:43:01 +0800

    create automated test for FormatCurrency in amazon project

---
+++ Amazon_Project/test/moneyTest.js
@@ -0,0 +1,8 @@
+import {FormatCurrency} from '../Scripts/Utils/Money.js';
+console.log("test suite: FormatCurrency")
+console.log('converts cents into dollars');
+console.log(FormatCurrency(2095)==="20.95")
+console.log("works with 0")
+console.log(FormatCurrency(0)==="0.00")
+console.log("round up to nearest cents")
+console.log(FormatCurrency(2000.5)==="20.01")
