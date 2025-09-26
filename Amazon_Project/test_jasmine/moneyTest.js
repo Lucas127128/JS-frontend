c6de4f25fb1e7ebcb64e2e60f2812a61df551397
Lucas Leung <kayauleung2024@gmail.com>
2025-09-27 00:39:56 +0800

    Do test of FormatCurrency function with Jasmine in Amazon project

---
+++ Amazon_Project/test_jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import { FormatCurrency } from "../Scripts/Utils/Money.js";
+
+describe("test suite: FormatCurrency", ()=>{
+    it('converts cents into dollars', ()=>{
+        expect(FormatCurrency(2095)).toEqual("20.95")
+    })
+    it("works with 0", ()=>{
+        expect(FormatCurrency(0)).toEqual("0.00")
+    })
+    it("round up to nearest cents", ()=>{
+        expect(FormatCurrency(2000.5)).toEqual("20.01")
+    })
+})
