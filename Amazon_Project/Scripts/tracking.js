53e5046139c1ff0a9c2533824943cb3e08e3313a
Lucas Leung <kayauleung2024@gmail.com>
2025-12-15 19:40:02 +0800

    Move the amazon project to Lucas127128/Amazon-clone

--- Amazon_Project/Scripts/tracking.js
+++
@@ -1,5 +0,0 @@
-const url = new URL(location.href);
-const orderId = url.searchParams.get("orderId");
-const prductId = url.searchParams.get("productId");
-console.log(orderId);
-console.log(prductId);
--- Amazon_Project/test-simple/moneyTest.js
+++
@@ -1,8 +0,0 @@
-import { formatCurrency } from "../Scripts/Utils/Money.js";
-console.log("test suite: FormatCurrency");
-console.log("converts cents into dollars");
-console.log(formatCurrency(2095) === "20.95");
-console.log("works with 0");
-console.log(formatCurrency(0) === "0.00");
-console.log("round up to nearest cents");
-console.log(formatCurrency(2000.5) === "20.01");
